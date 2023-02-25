584a8a5cadb5388ca76e3a1cb3695ac9c2b4bcab
aks <aakashksingh4@gmail.com>
2023-02-26 01:32:40 +0530

    deleted useless code. added methods to store all mails for training in a hdf file

--- read_mails.py
+++ read_mails.py
@@ -1,15 +1,40 @@
 from authorize import build_service
 import base64
 from bs4 import BeautifulSoup
+import pandas as pd
+from collections import defaultdict
 
 # Call the Gmail API
 service = build_service()
+TOTAL_MAILS = 700
+
+def list_labels():
+    results = service.users().labels().list(userId='me').execute()
+    labels = results.get('labels', [])
+
+    if not labels:
+        return []
+
+    label_list = [label['name'] for label in labels]
+    return label_list
+
+
+all_labels = list_labels()
 
 
 def read_n_mails(n=1):
+    """
+    give dataframe containing specified no of mails
+    :param n: int  #maximum 200
+    :return: pd.DataFrame
+    """
+
     result = service.users().messages().list(maxResults=n, userId='me').execute()
-    messages = result.get('messages')
     # messages is a list of dictionaries where each dictionary contains a message id.
+    messages = result.get('messages')
+    # dictionary to store mails
+    messages_dict = defaultdict(list)
+    labels_dict = {label: [] for label in all_labels}
 
     # iterate through all the messages
     for msg in messages:
@@ -47,12 +72,37 @@ def read_n_mails(n=1):
             # Now, the data obtained is in lxml. So, we will parse
             # it with BeautifulSoup library
             soup = BeautifulSoup(decoded_data, "lxml")
-            body = soup.body()
+            body = str(soup.body())[4:-5]
+            formatted_body = format_body(body)
 
-            # Printing the subject, sender's email and message
-            print("Subject: ", subject)
-            print("From: ", sender)
-            print("Message: ", body)
+            # adding data in the dictionary
+            messages_dict['id'].append(msg['id'])
+            messages_dict['date'].append(date)
+            messages_dict['sender'].append(sender)
+            messages_dict['receiver'].append(receiver)
+            messages_dict['subject'].append(subject)
+            messages_dict['body'].append(formatted_body)
+
+            # filling up labels as a numerical data
+            for label in labels:
+                labels_dict[label].append(1)
 
         except:
-            print('error')
+            pass
+
+    # dataframe to store the messages
+    messages_df = pd.DataFrame(messages_dict)
+    labels_df = pd.DataFrame(labels_dict)
+    mails_df = pd.concat([messages_df, labels_df])
+    return mails_df
+
+def format_body(body):
+    return body
+
+def write_all_mails_to_hdf():
+    lst_dfs = []
+    for mail_count in range(TOTAL_MAILS//200):
+        lst_dfs.append(read_n_mails(200))
+
+    all_mails_df = pd.concat(lst_dfs)
+    all_mails_df.to_hdf('./data/training_data.hdf')
