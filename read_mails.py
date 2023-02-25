c84ae8ddcde7955890c70eb28e6189cf9c3f65a2
aks <aakashksingh4@gmail.com>
2023-02-25 19:31:08 +0530

    created method to parse mails

--- read_mails.py
+++ read_mails.py
@@ -1,100 +1,58 @@
-import os.path
+from authorize import build_service
 import base64
 from bs4 import BeautifulSoup
-from google.auth.transport.requests import Request
-from google.oauth2.credentials import Credentials
-from google_auth_oauthlib.flow import InstalledAppFlow
-from googleapiclient.discovery import build
-from googleapiclient.errors import HttpError
 
-# If modifying these scopes, delete the file token.json.
-SCOPES = [
-    'https://www.googleapis.com/auth/gmail.modify',  # for modifying labels of messages
-    'https://www.googleapis.com/auth/gmail.labels'  # for managing labels
-]
+# Call the Gmail API
+service = build_service()
 
 
-def main():
+def read_n_mails(n=1):
+    result = service.users().messages().list(maxResults=n, userId='me').execute()
+    messages = result.get('messages')
+    # messages is a list of dictionaries where each dictionary contains a message id.
 
-    print('Authorize yourself')
+    # iterate through all the messages
+    for msg in messages:
+        # Get the message from its id
+        txt = service.users().messages().get(userId='me', id=msg['id']).execute()
 
-    creds = None
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    if os.path.exists('token.json'):
-        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
-    # If there are no (valid) credentials available, let the user log in.
-    if not creds or not creds.valid:
-        if creds and creds.expired and creds.refresh_token:
-            creds.refresh(Request())
-        else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                'credentials.json', SCOPES)
-            creds = flow.run_local_server(port=0)
-        # Save the credentials for the next run
-        with open('token.json', 'w') as token:
-            token.write(creds.to_json())
+        # Use try-except to avoid any Errors
+        try:
+            # Get value of 'payload' from dictionary 'txt'
+            payload = txt['payload']
+            headers = payload['headers']
+            # get list of labels of the email
+            labels = txt['labelIds']
 
-    print('Successfully Authorized !')
+            # Look for metadata of Email in the headers
+            # which is a list of dictionaries
+            for d in headers:
+                if d['name'] == 'Date':
+                    date = d['value']
+                if d['name'] == 'Subject':
+                    subject = d['value']
+                if d['name'] == 'From':
+                    sender = d['value']
+                if d['name'] == 'To':
+                    receiver = d['value']
 
-    try:
-        # Call the Gmail API
-        service = build('gmail', 'v1', credentials=creds)
-        result = service.users().messages().list(userId='me').execute()
+            # The Body of the message is in Encrypted format. So, we have to decode it.
+            # Get the data and decode it with base 64 decoder.
+            parts = payload.get('parts')[0]
+            data = parts['body']['data']
+            data = data.replace("-", "+").replace("_", "/")
+            # decrypting the message body
+            decoded_data = base64.b64decode(data)
 
-        # We can also pass maxResults to get any number of emails. Like this:
-        # result = service.users().messages().list(maxResults=200, userId='me').execute()
-        messages = result.get('messages')
+            # Now, the data obtained is in lxml. So, we will parse
+            # it with BeautifulSoup library
+            soup = BeautifulSoup(decoded_data, "lxml")
+            body = soup.body()
 
-        # messages is a list of dictionaries where each dictionary contains a message id.
+            # Printing the subject, sender's email and message
+            print("Subject: ", subject)
+            print("From: ", sender)
+            print("Message: ", body)
 
-        # iterate through all the messages
-        ctr=0
-        for msg in messages:
-            # Get the message from its id
-            txt = service.users().messages().get(userId='me', id=msg['id']).execute()
-
-            # Use try-except to avoid any Errors
-            try:
-                # Get value of 'payload' from dictionary 'txt'
-                payload = txt['payload']
-                headers = payload['headers']
-
-                # Look for Subject and Sender Email in the headers
-                for d in headers:
-                    if d['name'] == 'Subject':
-                        subject = d['value']
-                    if d['name'] == 'From':
-                        sender = d['value']
-
-                # The Body of the message is in Encrypted format. So, we have to decode it.
-                # Get the data and decode it with base 64 decoder.
-                parts = payload.get('parts')[0]
-                data = parts['body']['data']
-                data = data.replace("-","+").replace("_","/")
-                # deencrypting the message body
-                decoded_data = base64.b64decode(data)
-
-                # Now, the data obtained is in lxml. So, we will parse
-                # it with BeautifulSoup library
-                soup = BeautifulSoup(decoded_data , "lxml")
-                body = soup.body()
-
-                # Printing the subject, sender's email and message
-                print("Subject: ", subject)
-                print("From: ", sender)
-                print("Message: ", body)
-                print('\n')
-                ctr += 1
-                if ctr==2: break
-            except:
-                print('error')
-
-    except HttpError as error:
-        # TODO(developer) - Handle errors from gmail API.
-        print(f'An error occurred: {error}')
-
-
-if __name__ == '__main__':
-    main()
+        except:
+            print('error')
