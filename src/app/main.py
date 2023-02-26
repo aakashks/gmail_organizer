38c7c1766fc65dc13b60b94ce6f85f437dcd9475
aks <aakashksingh4@gmail.com>
2023-02-26 14:36:51 +0530

    organized files. added logging.

---
+++ src/app/main.py
@@ -0,0 +1,10 @@
+from src.lib.read_mails import store_all_mails
+import logging
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+if __name__ == '__main__':
+    logger.info('main program started')
+    store_all_mails()
+    logger.info('end program')
