859753c70b20b02c177a5d5b08cd2cb35cb2c89a
aks <aakashksingh4@gmail.com>
2023-03-05 22:17:42 +0530

    fixed tfidf. successfully generating 2d list of labels. implemented classes properly.

--- src/lib/ML.py
+++ src/lib/ML.py
@@ -1,23 +1,35 @@
+import json
+import logging
+import re
+from time import time
+from typing import List
+
+import joblib
 import numpy as np
 import pandas as pd
-import joblib
-from typing import List
-import logging
+from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.impute import SimpleImputer
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn.preprocessing import MultiLabelBinarizer
 from sklearn.pipeline import Pipeline
 from sklearn.base import BaseEstimator, TransformerMixin
-from sklearn.model_selection import StratifiedShuffleSplit, cross_val_score, GridSearchCV, RandomizedSearchCV, \
-    train_test_split
+from sklearn.model_selection import cross_val_score, GridSearchCV, RandomizedSearchCV, train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.compose import ColumnTransformer
-from sklearn.feature_extraction.text import TfidfVectorizer
-from sklearn.preprocessing import MultiLabelBinarizer
+
 from conf.user_info import USER_EMAIL_ID
-import json
-import re
 
 logger = logging.getLogger(__name__)
+
+
+def _encode_corpus_for_train(corpus: pd.Series, max_df=0.8, min_df=0.05) -> np.ndarray:
+    tfidf = TfidfVectorizer(
+        max_df=max_df,
+        min_df=min_df
+    )
+    encoded_corpus = tfidf.fit_transform(corpus).toarray()
+    joblib.dump(tfidf.vocabulary_, '../../data/tfidf_vocabulary.pkl')
+    return encoded_corpus
 
 
 class Preprocess:
@@ -43,21 +55,19 @@ class Preprocess:
         labels_array = [list(st.split(',')) for st in label_series]
         return self.mlb.transform(labels_array)
 
-    def _encode_corpus(self, corpus: pd.Series) -> np.ndarray:
-        self.tfidf = TfidfVectorizer(
-            max_df=0.8,
-            min_df=0.05
-        )
-        encoded_corpus = self.tfidf.fit_transform(corpus).toarray()
+    def _encode_corpus(self, message_body: pd.Series) -> np.ndarray:
+        vocab = joblib.load('../../data/tfidf_vocabulary.pkl')
+        self.tfidf = TfidfVectorizer(vocabulary=vocab)
+        encoded_corpus = self.tfidf.fit_transform(message_body).toarray()
         return encoded_corpus
 
     def get_encoded_corpus(self, df: pd.DataFrame) -> pd.DataFrame:
         final_df = self._clean_email_df(df)
         return pd.DataFrame(self._encode_corpus(final_df['body']))
-    
+
     def get_training_data(self, df: pd.DataFrame) -> (pd.DataFrame, pd.DataFrame):
         final_df = self._clean_email_df(df)
-        encoded_corpus_df = pd.DataFrame(self._encode_corpus(final_df['body']))
+        encoded_corpus_df = pd.DataFrame(_encode_corpus_for_train(final_df['body']))
         encoded_labels_df = pd.DataFrame(self._encode_labels(final_df['labels']))
         return encoded_corpus_df, encoded_labels_df
 
@@ -69,11 +79,11 @@ class GenerateLabels(Preprocess):
 
     def generate_labels(self, read_mails: pd.DataFrame) -> List[List[str]]:
         """
-        return a dataframe of labels for the given dataframe of mails
+        return labels for the given dataframe of mails
         """
         encoded_message = super().get_encoded_corpus(read_mails)
         encoded_labels = self._predict(encoded_message)
-        labels_list = super().mlb.inverse_transform(encoded_labels)
+        labels_list = self.mlb.inverse_transform(encoded_labels)
         return labels_list
 
     def _predict(self, encoded_message):
@@ -88,16 +98,21 @@ class FitModel(Preprocess):
     def __init__(self, df: pd.DataFrame, model_name='knn'):
         super().__init__()
         self.df = df
-    
+
     def knn_fit_and_dump(self):
-        X, y = super().get_training_data(self.df)
+        encoded_message_body_df, encoded_labels_df = self.get_training_data(self.df)
         knn_clf = KNeighborsClassifier()
-        knn_clf.fit(X, y)
-        logger.info('saving model')
+        logger.info('training model')
+        t0 = time()
+        knn_clf.fit(encoded_message_body_df, encoded_labels_df)
         joblib.dump(knn_clf, '../../data/knn_model.pkl')
-        logger.info('model saved!')
+        logger.info(f'model saved! took {time() - t0} seconds')
 
 
-df1 = pd.read_csv('../../data/training_data.csv', sep='~', index_col=0)
-knn_model = FitModel(df1)
-knn_model.knn_fit_and_dump()
+def train_and_dump_model():
+    df1 = pd.read_csv('../../data/training_data.csv', sep='~', index_col=0)
+    knn_model = FitModel(df1)
+    knn_model.knn_fit_and_dump()
+
+
+# train_and_dump_model()
